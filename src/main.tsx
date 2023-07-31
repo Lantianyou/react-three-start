17b120c3e66a02353633171e941c5e20c7da5c40
Lantianyou <dtlantianyou@gmail.com>
2023-07-31 12:28:40 +0800

    feat: react three fiber with tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { Canvas } from "@react-three/fiber";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <App position={[-1.2, 0, 0]} />
+      <App position={[1.2, 0, 0]} />
+    </Canvas>
   </React.StrictMode>,
-)
+);
